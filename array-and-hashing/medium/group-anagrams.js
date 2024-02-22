2f2456ae723b4d67022884064f5e6373adb8c16e
Justin Lo <kk123456235@gmail.com>
2024-02-23 01:36:37 +0800

    Array and hashing: Medium (Not-complete)

---
+++ array-and-hashing/medium/group-anagrams.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
+console.log(groupAnagrams([""])); // [[""]]
+console.log(groupAnagrams(["a"])); // [["a"]]

1c30f5a4c38387fa96603f69169ed4845ba06aae
Justin Lo <justin.lo@qupital.com>
2024-02-23 11:42:09 +0800

    Array and hashing: group-anagrams (Medium)

--- array-and-hashing/medium/group-anagrams.js
+++ array-and-hashing/medium/group-anagrams.js
@@ -3,7 +3,24 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-
+    const map = new Map();
+    for (const index in strs) {
+        const str = strs[index];
+        const orderedString = str.split('').sort().join('');
+        if (map.has(orderedString)){
+            const existing = map.get(orderedString);
+            existing.push(str);
+            map.set(orderedString, existing);
+        }
+        else {
+            map.set(orderedString, new Array(str));
+        }
+    }
+    const result = [];
+    for (const values of map.values()){
+        result.push(values);
+    }
+    return result;
 };
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]

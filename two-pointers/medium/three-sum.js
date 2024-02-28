0bb9a0fa2cfab0b51d692cb4c7eb6e2281bc25fc
Justin Lo <justin.lo@qupital.com>
2024-02-28 17:47:48 +0800

    Two pointers: three-sum (Not-complete)

---
+++ two-pointers/medium/three-sum.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4])) // [[-1,-1,2],[-1,0,1]]
+console.log(threeSum([0,1,1])) // []
+console.log(threeSum([0,0,0])) // [[0,0,0]]

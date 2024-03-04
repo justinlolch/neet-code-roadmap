21e09a83f8f7f57501fe3d4773f9a719a366df5e
Justin Lo <justin.lo@qupital.com>
2024-03-04 10:58:59 +0800

    Binary search: search-in-rotated-sorted-array (Medium)

--- binary-search/medium/search-in-rotated-sorted-array.js
+++ binary-search/medium/search-in-rotated-sorted-array.js
@@ -2,25 +2,11 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
- *
- * 2 3 5
- * sorted array left = minimum
- *
- * 3 5 2
- * left < middle, right < middle
- *
- * 5 2 3
- * left > middle, right > middle
- *
  */
 var search = function (nums, target) {
     let leftPt = 0;
     let rightPt = nums.length - 1;
-    if (nums.length === 1) {
-        if (target !== nums[0])
-            return -1;
-        else return 0;
-    }
+    let index = -1;
 
     while (leftPt <= rightPt) {
         const middlePt = (leftPt + rightPt) >> 1;
@@ -37,57 +23,56 @@ var search = function (nums, target) {
         if (rightValue === target)
             return rightPt;
 
-        if (target < middleValue && target < rightValue && target < leftValue) {
-            // search in small sorted array
-            if (leftValue > middleValue && rightValue > middleValue) {
-                leftPt += 1;
-                rightPt = middlePt - 1;
+        if (target < middleValue) {
+            if (middleValue > leftValue) {
+                // left to middle pointer is sorted
+                if (target > leftValue) {
+                    // target is between left and middle pointer
+                    leftPt += 1;
+                    rightPt = middlePt - 1;
+                } else {
+                    // target is between middle and right pointer
+                    leftPt = middlePt + 1;
+                    rightPt -= 1;
+                }
+            } else {
+                // right pointer is sorted
+                if (target < rightValue) {
+                    // target is between left and middle pointer
+                    leftPt += 1;
+                    rightPt = middlePt - 1;
+                } else return -1;
             }
-
-            if (leftValue < middleValue && rightValue < middleValue) {
-                leftPt = middlePt + 1;
-                rightPt -= 1;
-            }
-
-            if (leftValue < middleValue && leftValue < rightValue) {
-                rightPt = middlePt - 1;
-                leftPt += 1;
-            }
-
-            if (!(leftValue > middleValue && rightValue > middleValue) &&
-                !(leftValue < middleValue && rightValue < middleValue) &&
-                !(leftValue < middleValue && leftValue < rightValue))
-                return -1;
         } else {
-            // search in big array
-            if (leftValue > middleValue && rightValue > middleValue) {
-                leftPt = middlePt + 1;
-                rightPt -= 1;
+            if (rightValue > middleValue) {
+                // middle to right pointer sorted
+                if (target < rightValue) {
+                    // target is between middle and right pointer
+                    leftPt = middlePt + 1;
+                    rightPt -= 1;
+                } else {
+                    // target is between left and middle pointer
+                    leftPt += 1;
+                    rightPt = middlePt - 1;
+                }
+            } else {
+                // left to middle pointer is sorted
+                if (target > leftValue) {
+                    // target is between middle and right pointer
+                    rightPt -= 1;
+                    leftPt = middlePt + 1;
+                } else return -1;
             }
-
-            if (leftValue < middleValue && rightValue < middleValue) {
-                leftPt += 1;
-                rightPt = middlePt - 1;
-            }
-
-            if (leftValue < middleValue && leftValue < rightValue) {
-                rightPt -= 1;
-                leftPt = middlePt + 1;
-            }
-
-            if (
-                !(leftValue > middleValue && rightValue > middleValue) &&
-                !(leftValue < middleValue && rightValue < middleValue) &&
-                !(leftValue < middleValue && leftValue < rightValue)
-            ) leftPt += 1;
         }
-
     }
 
     return -1;
 };
 
-console.log(search([4,5,6,7,8,1,2,3], 8)) // 4
+console.log(search([3, 1], 2)) // -1
+console.log(search([8, 9, 2, 3, 4], 9)) // 1
+console.log(search([5, 1, 2, 3, 4], 1)) // 1
+console.log(search([4, 5, 6, 7, 8, 1, 2, 3], 8)) // 4
 console.log(search([4, 5, 6, 7, 0, 1, 2], 0)) // 4
 console.log(search([4, 5, 6, 7, 0, 1, 2], 5)) // 1
 console.log(search([4, 5, 6, 7, 0, 1, 2], 3)) // -1

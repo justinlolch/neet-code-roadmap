522e16ded04a7397c87d2de26fd6f84a8ade1f40
Justin Lo <justin.lo@qupital.com>
2024-03-13 11:59:38 +0800

    Algorithms and structures for beginners: combination-sum

--- course/algorithms-and-data-structures-for-beginners/backtracking/combination-sum.js
+++ course/algorithms-and-data-structures-for-beginners/backtracking/combination-sum.js
@@ -6,25 +6,29 @@
 var combinationSum = function (candidates, target) {
     const resultSet = [];
 
-    const subset = [];
-    const dfs = (i) => {
-        subset.push(i);
-        const sum = subset.reduce((val, acc) => acc += val, 0);
+    const dfs = (i, subset) => {
+        if (i >= candidates.length)
+            return;
+
+        const sum = subset.reduce((val,acc)=>acc+val,0);
+        if (sum > target)
+            return;
+
         if (sum === target) {
-            resultSet.push([...subset]);
+            resultSet.push([...subset])
             return;
         }
 
-        if (target < sum)
-            dfs(i);
-            else {
-               subset.pop();
-               dfs(i+1);
-        }
-    }
+        subset.push(candidates[i]);
+        dfs(i, subset);
+        subset.pop();
+        dfs(i+1, subset);
+    };
 
+    dfs(0, []);
     return resultSet;
 };
 
-console.log([2, 3, 6, 7], 7) // [[2,2,3],[7]]
-console.log([2, 3, 5], 8) // [[2,2,2,2],[2,3,3],[3,5]]
+console.log(combinationSum([3, 5, 7], 10)); // [[3,7],[5,5]]
+console.log(combinationSum([2, 3, 6, 7], 7)); // [[2,2,3],[7]]
+console.log(combinationSum([2, 3, 5], 8)); // [[2,2,2,2],[2,3,3],[3,5]]

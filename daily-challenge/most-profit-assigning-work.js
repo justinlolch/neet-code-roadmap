e11178e26b199a6688f3c8831195fd456ec12437
Justin Lo <kk123456235@gmail.com>
2024-06-18 11:21:55 +0100

    Daily challenge: most-profit-assigning-work (Medium)

---
+++ daily-challenge/most-profit-assigning-work.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} difficulty
+ * @param {number[]} profit
+ * @param {number[]} worker
+ * @return {number}
+ */
+var maxProfitAssignment = function (difficulty, profit, worker) {
+    worker.sort((a, b) => a - b);
+
+    let maxProfit = 0;
+    const maxProfitQueue = new MaxPriorityQueue({priority: elem => elem.pro});
+    const minDifficultyQueue = new MinPriorityQueue({priority: elem => elem.diff});
+
+    for (let i = 0; i < profit.length; i++) {
+        const elem = {pro: profit[i], diff: difficulty[i]};
+        minDifficultyQueue.enqueue(elem);
+    }
+
+    for (const workDiff of worker) {
+        while (!minDifficultyQueue.isEmpty() && minDifficultyQueue.front().element.diff <= workDiff) {
+            maxProfitQueue.enqueue(minDifficultyQueue.dequeue().element);
+        }
+        if (!maxProfitQueue.isEmpty()){
+            maxProfit += maxProfitQueue.front().element.pro;
+        }
+    }
+
+    return maxProfit;
+};
+
+console.log(maxProfitAssignment([2, 4, 6, 8, 10], [10, 20, 30, 40, 50], [4, 5, 6, 7])) // 100
+console.log(maxProfitAssignment([85, 47, 57], [24, 66, 99], [40, 25, 25])) // 0
+
+// 18/6/2024
